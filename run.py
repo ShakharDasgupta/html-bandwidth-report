e8854a00e0643572506a1cbfad5b99ce096ec071
ExcaliburZero <cwellsny@nycap.rr.com>
2015-10-30 20:18:27 -0400

    Set run script to use relative paths
    Changed the run script so that it uses relative paths in order to remove the need to change the absolute path when the main directory is placed elsewhere in the filesystem.

--- run.py
+++ run.py
@@ -3,14 +3,14 @@ from subprocess import Popen, PIPE
 import time
 
 # Start the server
-server_terminal = Popen("cd /home/chris/Documents/Code/html-bandwidth-report && python -m SimpleHTTPServer 8080", shell=True, stdin=PIPE, stdout=PIPE)
+server_terminal = Popen("python -m SimpleHTTPServer 8080", shell=True, stdin=PIPE, stdout=PIPE)
 
 # Run the get speed command evey x minutes y times
 x = 5
 y = 120
 i = 0
 while i < y:
-	Popen("cd /home/chris/Documents/Code/html-bandwidth-report && python speedtest_cli.py --csv bandwidth_report.csv", shell=True, stdin=PIPE, stdout=PIPE)
+	Popen("python speedtest_cli.py --csv bandwidth_report.csv", shell=True, stdin=PIPE, stdout=PIPE)
 	print("Got time: " + str(x * (i + 1)))
 	time.sleep(x * 60);
 	i = i + 1
